251d9de4dc21d878a8f7cb6587f9336e0a265715
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-10-01 16:04:01 +0200

    feat: test print projects in Portfolio

--- src/pages/Portfolio.vue
+++ src/pages/Portfolio.vue
@@ -26,9 +26,25 @@
 
 <template>
     <h1>Portfolio</h1>
+
+    <div class="cards-container">
+        <ul>
+            <li v-for="project in this.projects" :key="project.id">
+                {{ project.title }}
+            </li>
+        </ul>
+    </div>
 </template>
 
 <style scoped lang="scss">
+
+    .cards-container {
+        padding: 20px;
+        margin-top: 25px;
+        border-radius: 20px;
+        border: 1px solid var(--primary);
+        background-color: var(--background);
+    }
 
 </style>
 
