dd6273378c20ddf92fd8349523315096231e4875
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-10-04 16:59:38 +0200

    feat: add cards to projects per type & projects per technology

--- src/pages/ProjectsPerType.vue
+++ src/pages/ProjectsPerType.vue
@@ -29,14 +29,60 @@
 
 <template>
     <h1> {{ this.type.name }} </h1>
-    <ul>
-        <li v-for="project in this.projects" :key="project.id">
-            {{ project.title }}
-        </li>
-    </ul>
+    <div class="card-container">
+        <div class="row">
+            <div class="card" v-for="project in this.projects" :key="project.id">
+                <h3>
+                    {{ project.title }}
+                </h3>
+                <RouterLink :to="{name: 'ProjectDetails', params: {slug: project.slug}}">
+                    <i class="fa-solid fa-eye"></i>
+                </RouterLink>
+            </div>
+        </div>
+    </div>
 
 </template>
 
 <style scoped lang="scss">
+    .card-container {
+        width: 80%;
+        margin: 25px auto;
 
+        .row {
+            display: flex;
+            justify-content: center;
+            align-items: stretch;
+            flex-wrap: wrap;
+            gap: 30px;
+
+            .card {
+                width: calc((100% / 4) - 30px);
+                padding: 20px;
+                text-align: left;
+                border-radius: 20px;
+                border: solid 1px var(--secondary);
+                background-color: var(--background);
+                position: relative;
+                line-height: 1.5;
+                box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+
+                h3 {
+                    display: inline-block;
+                    max-width: 60%;
+                }
+
+                a {
+                    padding: 6px 10px;
+                    border-radius: 50%;
+                    background-color: var(--accent);
+                    position: absolute;
+                    bottom: 20px;
+                    right: 25px;
+                }
+
+            }
+        }
+
+    }
 </style>
